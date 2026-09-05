ecc0453a013bc84a4c7bb45edd232cfaa4efbbc0
Hamzaoui-Louai <l_hamzaoui@estin.dz>
2026-09-06 00:17:02 +0100

    added the fall icon

--- src/main/index.js
+++ src/main/index.js
@@ -49,7 +49,7 @@ function createWindow() {
     height: 670,
     show: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
+    icon,
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false
